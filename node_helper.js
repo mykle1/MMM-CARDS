b40b41509978e3e84784d52832bb55d20580289d
John Wade <cowboysdude@stny.rr.com>
2017-06-29 21:06:25 -0400

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -21,12 +21,7 @@ module.exports = NodeHelper.create({
             method: 'GET'
         }, (error, response, body) => {
             if (!error && response.statusCode == 200) {
-				var cards = JSON.stringify(cards, null, 2);
-				var result = JSON.parse(cards);
-        //        var result = JSON.parse(body).cards; // .cards // Parsing an array
-				
-				
-				console.log(response.statusCode + result);
+				var result = JSON.parse(body);
                     this.sendSocketNotification('CARDS_RESULT', result);
             }
         });
