50ecd705f10d761fa5f26f8e4c87d473ced90926
Mykle <themykle@imykle.com>
2017-07-02 08:53:12 -0400

    Add files via upload

--- MMM-CARDS.js
+++ MMM-CARDS.js
@@ -9,7 +9,8 @@ Module.register("MMM-CARDS", {
     // Module config defaults.
     defaults: {
         rotateInterval: 60 * 1000,      // New cards rotation.
-        header: "Five Card Stud",    // Any text you want
+		useHeader: true,                // False if you don't want a header      
+        header: "Cards",    // Any text you want. useHeader must be true
         maxWidth: "100%",
         animationSpeed: 3000,           // Cards fade in and out (speed)
         initialLoadDelay: 4250,
@@ -28,7 +29,7 @@ Module.register("MMM-CARDS", {
         requiresVersion: "2.1.0",
 
     //  Set locale.
-        this.url = "https://deckofcardsapi.com/api/deck/new/draw/?count=5";
+        this.url = "https://deckofcardsapi.com/api/deck/new/draw/?count=10";
         this.cards = [];
         this.activeItem = 0;
         this.rotateInterval = null;
@@ -48,17 +49,44 @@ Module.register("MMM-CARDS", {
         }
 		
 		// common header
-        var header = document.createElement("header");
-        header.classList.add("xsmall", "bright", "header");
-        header.innerHTML = this.config.header;
-        wrapper.appendChild(header);
+    //    var header = document.createElement("header");
+    //    header.classList.add("xsmall", "bright", "header");
+    //    header.innerHTML = this.config.header;
+    //    wrapper.appendChild(header);
+	
+	
+	if (this.config.useHeader != false) {
+            var header = document.createElement("header");
+            header.classList.add("xsmall", "bright", "light");
+            header.innerHTML = this.config.header;
+            wrapper.appendChild(header);
+        }
+	
+	
+	
 		
         var cards = this.cards;
 
         var top = document.createElement("div");
         top.classList.add("list-row");
-			
-		// card # 1
+		
+		
+		// The heading for your cards
+		var mirrorCards = document.createElement("div");
+		var mmCards = document.createElement("p");
+        mmCards.classList.add("xsmall", "bright", "spacer");
+        mmCards.innerHTML = "My cards are . . ."; // the fifth object is cards[4]
+		mirrorCards.appendChild(mmCards);
+		wrapper.appendChild(mirrorCards);
+		
+		
+		
+		
+		// The mirror is always the dealer. Every hand is a newly shuffled deck so
+		// I get the 1, 3, 5 ,7 and 9th card as it should be
+		// MM gets the 2, 4, 6, 8 and 10th card as it should be
+		
+		// My card # 1
         var pic = document.createElement("div");
         var img = document.createElement("img");
         img.classList.add("photo");
@@ -67,48 +95,118 @@ Module.register("MMM-CARDS", {
         wrapper.appendChild(pic);
 			
 		 	
-		// card #2
+		// My card #2
 		var pic2 = document.createElement("div");
         var img2 = document.createElement("img");
 		img2.classList.add("photo2");	
 		setTimeout(function() {
-            img2.src = cards[1].image; // the second object is cards[1]
+            img2.src = cards[2].image; // the second object is cards[1]
         }, 3 * 1000);
 		pic2.appendChild(img2);
 		wrapper.appendChild(pic2);
 			
 			
-		// card #3
+		// My card #3
 		var pic3 = document.createElement("div");
         var img3 = document.createElement("img");
 		img3.classList.add("photo3");	
 		setTimeout(function() {
-            img3.src = cards[2].image; // the third object is cards[2]
+            img3.src = cards[4].image; // the third object is cards[2]
         }, 5 * 1000);
 		pic3.appendChild(img3);
 		wrapper.appendChild(pic3);
 			
 			
-		// card #4
+		// My card #4
 		var pic4 = document.createElement("div");
         var img4 = document.createElement("img");
 		img4.classList.add("photo4");	
 		setTimeout(function() {
-            img4.src = cards[3].image; // the fourth object is cards[3]
+            img4.src = cards[6].image; // the fourth object is cards[3]
         }, 7 * 1000);
 		pic4.appendChild(img4);
 		wrapper.appendChild(pic4);	
 			
 			
-		// card #5
+		// My card #5
 		var pic5 = document.createElement("div");
         var img5 = document.createElement("img");
 		img5.classList.add("photo5");	
 		setTimeout(function() {
-            img5.src = cards[4].image; // the fifth object is cards[4]
+            img5.src = cards[8].image; // the fifth object is cards[4]
         }, 9 * 1000);
 		pic5.appendChild(img5);
-		wrapper.appendChild(pic5);		
+		wrapper.appendChild(pic5);
+		
+		
+		
+		// The heading between my cards and the mirror's cards
+		var mirrorCards = document.createElement("div");
+		var mmCards = document.createElement("p");
+        mmCards.classList.add("xsmall", "bright", "spacer");
+	    setTimeout(function() {
+            mmCards.innerHTML = "Magic Mirror's Cards"; // the fifth object is cards[4]
+        }, 11 * 1000);
+		mirrorCards.appendChild(mmCards);
+		wrapper.appendChild(mirrorCards);
+
+		
+		
+		// The mirror's cards start here
+		
+		// MM card # 1 
+        var pic6 = document.createElement("div");
+        var img6 = document.createElement("img");
+        img6.classList.add("photo6");
+        setTimeout(function() {
+            img6.src = cards[1].image; // the fifth object is cards[4]
+        }, 13 * 1000);
+		pic6.appendChild(img6);
+		wrapper.appendChild(pic6);
+			
+		 	
+		// MM card #2
+		var pic7 = document.createElement("div");
+        var img7 = document.createElement("img");
+		img7.classList.add("photo7");	
+		setTimeout(function() {
+            img7.src = cards[3].image; // the second object is cards[1]
+        }, 15 * 1000);
+		pic7.appendChild(img7);
+		wrapper.appendChild(pic7);
+			
+			
+		// MM card #3
+		var pic8 = document.createElement("div");
+        var img8 = document.createElement("img");
+		img8.classList.add("photo8");	
+		setTimeout(function() {
+            img8.src = cards[5].image; // the third object is cards[2]
+        }, 17 * 1000);
+		pic8.appendChild(img8);
+		wrapper.appendChild(pic8);
+			
+			
+		// MM card #4
+		var pic9 = document.createElement("div");
+        var img9 = document.createElement("img");
+		img9.classList.add("photo9");	
+		setTimeout(function() {
+            img9.src = cards[7].image; // the fourth object is cards[3]
+        }, 19 * 1000);
+		pic9.appendChild(img9);
+		wrapper.appendChild(pic9);	
+			
+			
+		// MM card #5
+		var pic10 = document.createElement("div");
+        var img10 = document.createElement("img");
+		img10.classList.add("photo10");	
+		setTimeout(function() {
+            img10.src = cards[9].image; // the fifth object is cards[4]
+        }, 21 * 1000);
+		pic10.appendChild(img10);
+		wrapper.appendChild(pic10);
 			
         return wrapper;
     },
